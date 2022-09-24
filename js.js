b8133613216273e2ab40ef84074807796b14a1b6
MrDen41k <ShumeldaD4@gmail.com>
2022-09-24 10:29:42 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,10 @@
+let garne = +prompt('введіть ЧиСлО');
+if (garne % 5 === 0) {
+    console.log("число хороше")
+}
+if (garne % 15 === 0) {
+    console.log("число хороше")
+}
+else {
+    console.log("число нехороше")
+}
